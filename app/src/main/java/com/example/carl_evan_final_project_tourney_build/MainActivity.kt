6234d08eef041629861941e5e86ab3e186e73c4d
Evan595 <53194327+Evan595@users.noreply.github.com>
2019-08-06 10:42:08 -0500

    orginal

---
+++ app/src/main/java/com/example/carl_evan_final_project_tourney_build/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.carl_evan_final_project_tourney_build
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
