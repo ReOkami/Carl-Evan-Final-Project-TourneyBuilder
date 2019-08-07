42dcb20ff194cefcb0a88557ca4e8c562dff56e4
Evan595 <53194327+Evan595@users.noreply.github.com>
2019-08-07 13:59:14 -0500

    day 2 in class v2

---
+++ app/src/main/java/okami/yami/carl_finalproject_tourneybuilder/DataModel.kt
@@ -0,0 +1,40 @@
+package okami.yami.carl_finalproject_tourneybuilder
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class DataModel(
+    @SerializedName("data")
+    @Expose
+    val `data`: List<Data>,
+    @SerializedName("page")
+    @Expose
+    val page: Int,
+    @SerializedName("perPage")
+    @Expose
+    val perPage: Int,
+    @SerializedName("total")
+    @Expose
+    val total: Int,
+    @SerializedName("totalPages")
+    @Expose
+    val totalPages: Int
+)
+
+data class Data(
+    @SerializedName("biography")
+    @Expose
+    val biography: String,
+    @SerializedName("id")
+    @Expose
+    val id: Int,
+    @SerializedName("name")
+    @Expose
+    val name: String,
+    @SerializedName("previousWins")
+    @Expose
+    val previousWins: String,
+    @SerializedName("tournamentName")
+    @Expose
+    val tournamentName: String
+)
