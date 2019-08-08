18f4c6b99e55e3df5d7c6e1935441b0a0da1a0cb
Evan595 <53194327+Evan595@users.noreply.github.com>
2019-08-08 08:51:50 -0500

    day 3 in class v1

--- app/src/main/java/okami/yami/carl_finalproject_tourneybuilder/RecyclerViewFragment.kt
+++ app/src/main/java/okami/yami/carl_finalproject_tourneybuilder/RecyclerViewFragment.kt
@@ -1,14 +1,19 @@
 package okami.yami.carl_finalproject_tourneybuilder
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_recycler_view.*
 import kotlinx.android.synthetic.main.fragment_screen_bio.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RecyclerViewFragment: Fragment(){
     override fun onCreateView(
@@ -39,10 +44,48 @@ class RecyclerViewFragment: Fragment(){
                 this.layoutManager = LinearLayoutManager(it)
             }
         }
-//        fetchDataFromServer(adapter)
+   fetchDataFromServer(adapter)
+    }
+
+    private fun fetchDataFromServer(adapter: RecyclerViewAdaptor) {
+
+        val apiCalls = RetroFitInstance.retrofit
+        val request = apiCalls.create(Endpoints::class.java).getParticipantList()
+
+        request.enqueue(object : Callback<List<Participant>> {
+
+            override fun onFailure(call: Call<List<Participant>>, t: Throwable) {
+                // Logcat Warn
+                Log.w(javaClass.name, "getParticipantList() failed. Error: ${t.message}")
+                // Show pop up if Fragment is still in view
+                constraintLayout?.let {
+                    Snackbar.make(it, "Network request failed.", Snackbar.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onResponse(call: Call<List<Participant>>, response: Response<List<Participant>>) {
+
+                when (response.code()) {
+
+                    200 -> {
+
+                        response.body()?.let {
+
+                            adapter.submitList(it)
+                        }
+                    }
+
+                    else -> {
+                        constraintLayout?.let {
+                            Snackbar.make(it, "Something went wrong. CODE: ${response.code()}", Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        })
     }
     }
 
-//}
+
 
 
